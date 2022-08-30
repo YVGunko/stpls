2855ba1e2c1396f6195ed7d92512002b3fc5607d
“YVGunko“ <yurygunko@gmail.com>
2022-08-30 13:25:28 +0300

    init

---
+++ src/main/java/com/yg/stpls/StplsApplication.java
@@ -0,0 +1,13 @@
+package com.yg.stpls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StplsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StplsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yg/stpls/StplsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.stpls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StplsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
